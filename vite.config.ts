fec5a996b775760eafe6239a4a22655e617b1fd4
parsehex <1779937+parsehex@users.noreply.github.com>
2025-09-22 11:00:10 -0400

    lowercase url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/DataKit/',
+	base: '/datakit/',
 	plugins: [vue(), tailwindcss()],
 	resolve: {
 		alias: {
